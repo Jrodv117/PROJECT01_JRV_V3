2262d3f01c7f8e7d2bac7b62c599a62589c2fc7e
jrodr117 <jrodr113@msudenver.edu>
2022-12-06 12:31:55 -0700

    Before feed back from professor

--- main.py
+++ main.py
@@ -1,206 +1,166 @@
 # Julian Rodriguez-Vera
-# Team Manager v2
-# 11/10/2022
-# This programn creates a simple program that allows user to manage his/her baseball team.
-
-import FileIO
-from datetime import datetime, date
+# Team Manager v3
+# 11/17/2022
+# This programn creates an OPP approach to a baseball team manager using a Three-Tier-Architecture
 
 
-def title():
-    print("========================================================")
-    print("\t\tBaseball Team Manager\n")
-    today_date = date.today()
-    print("CURRENT DATE :\t", today_date)
-    game_date_str = input("GAME DATE :\t")
-
-    if game_date_str != "" or len(game_date_str) != 0:
-        game_date = datetime.strptime(game_date_str, "%Y-%m-%d")
-        until_game_date = game_date - datetime(
-            today_date.year, today_date.month, today_date.day
-        )
-        print("DAYS UNTIL GAME : ", until_game_date.days)
-    menu()
-    print("POSITIONS")
-    print("C, 1B, 2B, 3B, SS, LF, CF, RF, P")
-    print("========================================================")
+import datetime
+from db import *
+from objects import *
 
 
-def menu():
-    print("\nMENU OPTIONS")
-    print(
-        "1 - Display lineup\n"
-        + "2 - Add player\n"
-        + "3 - Remove player\n"
-        + "4 - Move player\n"
-        + "5 - Edit player position\n"
-        + "6 - Edit player stats \n"
-        + "7 - Exit program\n"
-    )
+class Baseball:
+    def __init__(self):
+        self.positions_list = ("C", "1B", "2B", "3B", "SS", "LF", "C", "RF", "P")
 
+    def menu(self):
+        print("\nMENU OPTIONS")
+        print("1 - Display Lineup")
+        print("2 - Add Player")
+        print("3 - Remove Player")
+        print("4 - Move Player")
+        print("5 - Edit player position")
+        print("6 - Edit Player stats")
+        print("7 - Exit Program\n")
+        print("POSITIONS")
+        print(self.position_list())
+        print("================================================================\n")
 
-print()
+    def position_list(self):
+        """
+        Use tuple
+        """
+        return "{}, {}, {}, {}, {}, {}, {}, {}, {}".format(*self.positions_list)
 
+    def get_name(self):
+        first_name = str(input("Enter Name(First name): "))
+        last_name = str(input("Enter Name(Last name): "))
+        return first_name, last_name
 
-def display_lineup(player_information):
-    print(
-        " " * 3
-        + f"{'Player'.ljust(31)}{'POS'.ljust(6)}{'AB'.ljust(6)}{'H'.ljust(6)}{'AVG'.ljust(6)}"
-    )
-    print("=" * 60)
-    for idx, player_information in enumerate(player_information, 1):
-        number = str(idx)
-        name = player_information["name"]
-        position = player_information["position"]
-        at_bats = player_information["at_bats"]
-        hits = player_information["hits"]
-        avg = int(hits) / int(at_bats)
-        number = number.ljust(3)
-        name = name.ljust(31)
-        position = position.ljust(6)
-        at_bats = at_bats.ljust(6)
-        hits = hits.ljust(6)
-        avg = "{0:.3f}".format(avg).ljust(6)
-        print(number + name + position + at_bats + hits + avg)
-
-
-def add_player(player_information, valid_positions):
-    name = input("\nEnter player name: ").capitalize()
-    position = input("Enter Player Position: ").upper()
-    while position not in valid_positions:
-        position = input("Enter a valid position: ").upper()
-    at_bats = int(input("Enter player at_bats: "))
-    while at_bats <= -1:
-        at_bats = int(input("At bats cant be less than 0. Enter At bats: "))
-    hits = int(input("Enter player hits: "))
-    while hits > at_bats:
-        hits = int(input("Hits can't be greater than At bats. Enter Hits: "))
-    player_information.append(
-        {"name": name, "position": position, "at_bats": str(at_bats), "hits": str(hits)}
-    )
-    FileIO.save_to_csv(player_information)
-    print(f"{name} was added")
-
-
-def remove_player(player_information):
-    name = input("\nEnter player name to delete: ").capitalize()
-    while not any(d["name"] == name for d in player_information):
-        name = input("Enter a valid player to be edited : ").capitalize()
-    for i in range(0, len(player_information)):
-        if player_information[i]["name"].strip() == name.strip():
-            player_information.pop(i)
+    def get_position(self):
+        while True:
+            position = input("Position: ").strip().upper()
+            if position not in self.positions_list:
+                print("ERROR - Invalid position. Try again.")
+                continue
             break
-    print(f"{name} was removed.")
-    FileIO.save_to_csv(player_information)
+        return position
 
-
-def move_player(player_information):
-    player = input("\nEnter the name of player to be moved: ").capitalize()
-    while not any(d["name"] == player for d in player_information):
-        player = input("Enter a valid player to be edited : ").capitalize()
-    position_in_list = int(input("Enter new list order for the player: "))
-    for i in range(0, len(player_information)):
-        if player_information[i]["name"].strip() == player.strip():
-            deleted = player_information.pop(i)
-            player_information.insert(position_in_list - 1, deleted)
-            break
-    print(f"{player} was moved.")
-    FileIO.save_to_csv(player_information)
-
-
-def edit_player_position(player_information, valid_positions):
-    player = input("\nEnter the name of player to be edited : ").capitalize()
-    while not any(d["name"] == player for d in player_information):
-        player = input("Enter a valid player to be edited : ").capitalize()
-    position = input("Enter a new position: ").upper()
-    while position not in valid_positions:
-        position = input("Enter a valid position: ").upper()
-    for i in range(0, len(player_information)):
-        if player_information[i]["name"].strip() == player.strip():
-            player_information[i]["position"] = position
-            print(
-                player
-                + "'s position was changed to "
-                + player_information[i]["position"]
-            )
-            break
-    FileIO.save_to_csv(player_information)
-
-
-def get_at_bats(player_information, player):
-    for i in range(0, len(player_information)):
-        if player_information[i]["name"].strip() == player.strip():
-            player_at_bats = player_information[i]["at_bats"]
-    return player_at_bats
-
-
-def edit_player_stats(player_information):
-    player = input("\nEnter the name of player: ").capitalize()
-    while not any(d["name"] == player for d in player_information):
-        player = input("Enter a valid player to be edited: ").capitalize()
-    print("You selected ", player)
-    edit = input(f"Edit AB for {player}?(y/n): ").lower()
-    while not edit == "y" and not edit == "n":
-        edit = input(f"Edit AB for {player}?(y/n): ").lower()
-    if edit == "y":
-        at_bats = int(input("Enter new AB: "))
-        while at_bats <= -1:
-            at_bats = int(input("At bats cant be less than 0. Enter new AB: "))
-        for i in range(0, len(player_information)):
-            if player_information[i]["name"].strip() == player.strip():
-                player_information[i]["at_bats"] = str(at_bats)
+    def get_at_bats(self):
+        at_bats = 0
+        while True:
+            try:
+                at_bats = input("At bats: ")
+                if at_bats == "zero":
+                    at_bats = 0
+                    print("Using At bats amount of 0")
+                    break
+                at_bats = int(at_bats)
+                if at_bats < 0:
+                    print("ERROR - Don't put negative number. Try Again")
+                    continue
                 break
-        print(f"{player}'s AB were changed")
-    elif edit == "n":
-        pass
-    edit = input(f"Edit H for {player}?(y/n): ").lower()
-    while not edit == "y" and not edit == "n":
-        edit = input(f"Edit H for {player}?(y/n): ").lower()
-    if edit == "y":
-        hits = int(input("enter new H: "))
-        at_bats = get_at_bats(player_information, player)
-        while hits > int(at_bats):
-            hits = int(input("Hits can't be greater than AB. Enter new H: "))
-        for i in range(0, len(player_information)):
-            if player_information[i]["name"].strip() == player.strip():
-                player_information[i]["hits"] = str(hits)
+            except ValueError:
+                print("ERROR - Don't put string. Try again.")
+        return at_bats
+
+    def get_hits(self, at_bats):
+        hits = 0
+        while True:
+            try:
+                hits = int(input("Hits: "))
+                if hits < 0:
+                    print("ERROR - Don't put negative number. Try Again.")
+                    continue
+                if hits > at_bats:
+                    print("ERROR - Player cannot have more hits than at bats.")
+                    continue
                 break
-        print(f"{player}'s hits were changed")
-    elif edit == "n":
-        pass
-    FileIO.save_to_csv(player_information)
+            except ValueError:
+                print("ERROR - Don't put string. Try again.")
+        return hits
+
+    def add_player(self):
+        first_name, last_name = self.get_name()
+        position = self.get_position()
+        at_bats = self.get_at_bats()
+        hits = self.get_hits(at_bats)
+        return Player(first_name, last_name, position, at_bats, hits)
+
+    def get_number(self, prompt=""):
+        number = 0
+        while True:
+            try:
+                number = int(input(prompt))
+                if number < 0:
+                    print("ERROR - Don't put negative number. Try Again.")
+                    continue
+                break
+            except ValueError:
+                print("ERROR - Don't put string. Try again.")
+        return number
+
+
+def list_to_string(list):
+    string = ""
+    for element in list:
+        string += element
+    return string
 
 
 def main():
-    title()
 
-    player_information = FileIO.open_read_csv()
+    team = Baseball()
+    data_driver = db()
+    database = data_driver.load_from_file("players.txt")
+    lineup = Lineup()
+    for data in database:
+        first_name = data[0].split()
+        fname = list_to_string(first_name)
+        last_name = data[1].split()
+        lname = list_to_string(last_name)
+        position = data[2]
+        at_bats = int(data[3])
+        hits = int(data[4])
 
-    valid_positions = ("C", "1B", "2B", "3B", "SS", "LF", "CF", "RF", "P")
+        player = Player(fname, lname, position, at_bats, hits)
+        lineup.add(player)
+    print("================================================================")
+    print("Baseball Team Manager\n")
+    team.menu()
     while True:
+        choice = 0
         try:
-            user_selection = int(input("\nMenu option: "))
-            if user_selection == 1:
-                display_lineup(player_information)
-            elif user_selection == 2:
-                add_player(player_information, valid_positions)
-            elif user_selection == 3:
-                remove_player(player_information)
-            elif user_selection == 4:
-                move_player(player_information)
-            elif user_selection == 5:
-                edit_player_position(player_information, valid_positions)
-            elif user_selection == 6:
-                edit_player_stats(player_information)
-            elif user_selection == 7:
-                print("Bye!")
-                exit()
-            else:
-                print("Please input a valid menu selection\n")
-                menu()
-        except (ValueError, TypeError):
-            print("Please input a valid menu selection\n")
-            menu()
+            choice = int(input("\nMenu option: "))
+        except Exception:
+            print("\nInvalid Choice")
+            continue
+        if choice == 1:
+            lineup.display_lineup()
+        elif choice == 2:
+            lineup.add(team.add_player())
+        elif choice == 3:
+            lineup.remove(team.get_number("Number: "))
+        elif choice == 4:
+            number1 = team.get_number("Current lineup number: ")
+            number2 = team.get_number("New lineup number: ")
+            lineup.move(number1, number2)
+        elif choice == 5:
+            number = team.get_number("Lineup number: ")
+            position = team.get_position()
+            lineup.edit_player_position(number, position)
+        elif choice == 6:
+            number = team.get_number("Lineup number: ")
+            at_bats = team.get_at_bats()
+            hits = team.get_hits(at_bats)
+            lineup.edit_player_stats(number, at_bats, hits)
+        elif choice == 7:
+            data_driver.save_to_file("players.txt", lineup.get_players_db())
+            print("Bye!")
+            break
+        else:
+            print()
+            print("Invalid option. Try again!")
 
 
 if __name__ == "__main__":
--- objects.py
+++ objects.py
@@ -1,20 +1,107 @@
 class Player:
-    def __init__(self, first, last, position, at_bats, hits):
-        self.first = first
-        self.last = last
-        self.position = position 
+    def __init__(self, first_name, last_name, position, at_bats, hits):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.position = position
         self.at_bats = at_bats
         self.hits = hits
 
-    def full_name(self):
-        return self.first + '' + self.last
-    
-    def average(self):
-        average = self.hits / self.at_bats
-        average = "{0:.3f}".format(average).ljust(6)
-        return average
-    
+    def get_full_name(self):
+        return self.first_name + " " + self.last_name
+
+    def get_AVG(self):
+        if self.at_bats == 0:
+            average = 0
+        elif self.at_bats == 0 and self.hits == 0:
+            average = 0
+        else:
+            average = self.hits / self.at_bats
+        return round(average, 3)
+
+    def __str__(self):
+        return f"From str method of Test: a is {self.first_name}, b is {self.last_name},{self.position},{self.at_bats},{self.hits}"
+
+
 class Lineup:
     def __init__(self):
-        self.__lineup = []
-        
+        self.__starting_lineup = []
+
+    def check_number(self, number):
+        if 1 <= number <= len(self.__starting_lineup):
+            return True
+        return False
+
+    def add(self, player):
+        self.__starting_lineup.append(player)
+
+    def remove(self, number):
+        if self.check_number(number):
+            print(self.__starting_lineup[number - 1].get_full_name(), "was deleted.")
+            del self.__starting_lineup[number - 1]
+            return True
+        else:
+            print("\nInvalid number")
+            return False
+
+    def move(self, number1, number2):
+        if self.check_number(number1) and self.check_number(number2):
+            player1 = self.__starting_lineup[number1 - 1]
+            player2 = self.__starting_lineup[number2 - 1]
+            print(player1.get_full_name(), "was selected")
+            print(player1.get_full_name(), "was moved")
+            self.__starting_lineup[number1 - 1], self.__starting_lineup[number2 - 1] = (
+                self.__starting_lineup[number2 - 1],
+                self.__starting_lineup[number1 - 1],
+            )
+            return True
+        else:
+            print("\nInvalid number")
+            return False
+
+    def edit_player_position(self, number, position):
+        if self.check_number(number):
+            player = self.__starting_lineup[number - 1]
+            print(
+                "You selected {0} POS = {1}".format(
+                    player.get_full_name(), player.position
+                )
+            )
+            player.position = position
+            print(player.get_full_name(), "was updated.")
+
+    def edit_player_stats(self, number, at_bats, hits):
+        if self.check_number(number):
+            player = self.__starting_lineup[number - 1]
+            print(
+                "You selected {0} At bats = {1}, Hits = {2}".format(
+                    player.get_full_name(), player.at_bats, player.hits
+                )
+            )
+            player.at_bats = at_bats
+            player.hits = hits
+            print("Edit stats")
+
+    def display_lineup(self):
+        print(" Player\t\t\t POS AB H AVG")
+        print("================================================================")
+        for i, player in enumerate(self.__starting_lineup):
+            print("{:<3d}".format(i + 1), end="")
+            print("{:30s}".format(player.get_full_name()), end="")
+            print("{:>3s}".format(player.position), end="")
+            print("{:5d}".format(player.at_bats), end="")
+            print("{:5d}".format(player.hits), end="")
+            print("{:8.3f}".format(player.get_AVG()))
+
+    def get_players_db(self):
+        result = []
+        for player in self.__starting_lineup:
+            result.append(
+                [
+                    player.first_name,
+                    player.last_name,
+                    player.position,
+                    player.at_bats,
+                    player.hits,
+                ]
+            )
+        return result
